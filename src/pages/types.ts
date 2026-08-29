de6554debbb0f59cd059899b33c9eba39218233c
Rafael Oviedo <oviedo.rp@gmail.com>
2026-08-29 20:17:23 -0300

    fix: config for imports not to brake

--- src/pages/types.ts
+++ src/pages/types.ts
@@ -1,12 +1,10 @@
 
 export type PageProps = {
   document: Document;
-  navigate: (route: Routes) => void;
+  navigate: (route: RoutesNames) => void;
 }
 
 export enum RoutesNames {
   HOME = 'home',
   EXAMPLE = 'example',
 }
-
-type Routes = RoutesNames.HOME | RoutesNames.EXAMPLE;
