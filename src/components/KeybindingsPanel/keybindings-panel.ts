8a26941ce1d69a83d0f6c899ae2845bb1950f9ad
Rafael Oviedo <oviedo.rp@gmail.com>
2026-09-23 18:16:39 -0300

    chore: updates display version to 1.3.0

--- src/components/KeybindingsPanel/keybindings-panel.ts
+++ src/components/KeybindingsPanel/keybindings-panel.ts
@@ -84,7 +84,7 @@ export function ensureKeybindingsPanelDefined(window: TermWindow): void {
             <span class="keybindings-panel__key">Keybindings: ?</span>
           </div>
         </div>
-        <span>v1.2.0</span>
+        <span>v1.3.0</span>
       `;
     }
 
